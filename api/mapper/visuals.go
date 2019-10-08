93ac8627f7f73c188d6500f863a98b0b08659ee4
Andre Kupka <me-github@andrekupka.de>
2019-10-08 20:45:35 +0200

    return visuals with group ids in visual collection

--- api/mapper/visuals.go
+++ api/mapper/visuals.go
@@ -11,13 +11,18 @@ type VisualJSON struct {
 	ShowId uuid.UUID `json:"showId"`
 }
 
-type VisualWithGroupsJson struct {
+type VisualWithGroupsJSON struct {
 	VisualJSON
 	Groups []GroupJSON `json:"groups"`
 }
 
+type VisualWithGroupIdsJSON struct {
+	VisualJSON
+	GroupIds []uuid.UUID `json:"groupIds"`
+}
+
 type VisualCollectionJSON struct {
-	Visuals []VisualJSON `json:"visuals"`
+	Visuals []VisualWithGroupIdsJSON `json:"visuals"`
 }
 
 func MapVisual(showId uuid.UUID, visual *shows.Visual) VisualJSON {
@@ -28,8 +33,8 @@ func MapVisual(showId uuid.UUID, visual *shows.Visual) VisualJSON {
 	}
 }
 
-func MapVisualWithGroups(showId uuid.UUID, visual *shows.Visual) VisualWithGroupsJson {
-	data := VisualWithGroupsJson{
+func MapVisualWithGroups(showId uuid.UUID, visual *shows.Visual) VisualWithGroupsJSON {
+	data := VisualWithGroupsJSON{
 		VisualJSON: MapVisual(showId, visual),
 		Groups:     make([]GroupJSON, len(visual.Groups())),
 	}
@@ -41,12 +46,25 @@ func MapVisualWithGroups(showId uuid.UUID, visual *shows.Visual) VisualWithGroup
 	return data
 }
 
+func MapVisualWithGroupIds(showId uuid.UUID, visual *shows.Visual) VisualWithGroupIdsJSON {
+	data := VisualWithGroupIdsJSON{
+		VisualJSON: MapVisual(showId, visual),
+		GroupIds:   make([]uuid.UUID, len(visual.Groups())),
+	}
+
+	for i, group := range visual.Groups() {
+		data.GroupIds[i] = group.ID
+	}
+
+	return data;
+}
+
 func MapAllVisualsFromShows(shows []*shows.Show) VisualCollectionJSON {
-	var visuals = make([]VisualJSON, 0)
+	var visuals = make([]VisualWithGroupIdsJSON, 0)
 
 	for _, show := range shows {
 		for _, visual := range show.Visuals() {
-			visuals = append(visuals, MapVisual(show.ID, visual))
+			visuals = append(visuals, MapVisualWithGroupIds(show.ID, visual))
 		}
 	}
 
